f53815c0d5e2cbacf2354465d81ee3eaa505ae88
Laust <laust.bonnesen@mail.com>
2022-12-16 17:18:43 +0100

    Script src updated

--- src/main/resources/static/javascripts/renderings/script_rendering.js
+++ src/main/resources/static/javascripts/renderings/script_rendering.js
@@ -5,14 +5,16 @@ function renderScripts() {
 <!-- Scripts -->
 <script src="../static/javascripts/url.js"></script>
 
-<script src="../static/javascripts/renderings/head_content_rendering.js"></script>
-<script src="../static/javascripts/renderings/header_content_rendering.js"></script>
-<script src="../static/javascripts/renderings/main_content_rendering.js"></script>
-<script src="../static/javascripts/renderings/footer_content_rendering.js"></script>
-
-<script src="../static/javascripts/logics/login.js"></script>
 <script src="../static/javascripts/logics/liszt.js"></script>
+<script src="../static/javascripts/logics/login.js"></script>
 <script src="../static/javascripts/logics/session/saves/saveUser.js"></script>
 <script src="../static/javascripts/logics/session/gets/getUser.js"></script>
+
+<script src="../static/javascripts/renderings/head_content_rendering.js"></script>
+<script src="../static/javascripts/renderings/header_content_rendering.js"></script>
+<script src="../static/javascripts/renderings/footer_content_rendering.js"></script>
+<script src="../static/javascripts/renderings/main_content_renderings/login_rendering.js"
+<script src="../static/javascripts/renderings/main_content_renderings/main_content_rendering.js"></script>
+
     `;
 }
